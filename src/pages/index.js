57c5837ec83baf2b9316947ba0c1aa0d9e9bad40
mostafab <mostafa0104@gmail.com>
2022-12-16 12:57:19 -0500

    Format

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,22 @@ import { getServerSideUser } from "api/user";
 
 const IndexPage = () => <LoginPage />;
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  () => {
-    return async (ctx) => {
-      const user = await getServerSideUser(ctx.req);
-      if (user) {
-        return {
-          redirect: {
-            destination: "/tournaments",
-          },
-        };
-      }
+export const getServerSideProps = wrapper.getServerSideProps(() => {
+  return async (ctx) => {
+    const user = await getServerSideUser(ctx.req);
+    if (user) {
       return {
-        props: {
-          title: 'Welcome to Neg 5'
-        }
-      }
+        redirect: {
+          destination: "/tournaments",
+        },
+      };
+    }
+    return {
+      props: {
+        title: "Welcome to Neg 5",
+      },
     };
-  }
-);
+  };
+});
 
 export default IndexPage;
