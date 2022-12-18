5a618d783e70ac77964507285cb776d2421d8988
mostafab <mostafa0104@gmail.com>
2022-12-17 21:22:14 -0500

    Fix image CSP

--- src/components/common/modal/index.jsx
+++ src/components/common/modal/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import BootstrapModal from "react-bootstrap/Modal";
 
-const Modal = ({ onHide, children, title }) => {
+const Modal = ({ onHide, children, title, fullscreen = false }) => {
   return (
-    <BootstrapModal show centered onHide={onHide}>
+    <BootstrapModal show centered fullscreen={fullscreen} onHide={onHide}>
       <BootstrapModal.Header closeButton>
-        <BootstrapModal.Title>{title}</BootstrapModal.Title>
+        <BootstrapModal.Title className="h5">{title}</BootstrapModal.Title>
       </BootstrapModal.Header>
       <BootstrapModal.Body>{children}</BootstrapModal.Body>
     </BootstrapModal>
