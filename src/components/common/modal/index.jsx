aac6f79ef52a1bfc891414b476bf101b3af11089
mostafab <mostafa0104@gmail.com>
2022-12-19 17:49:40 -0500

    classname

--- src/components/common/modal/index.jsx
+++ src/components/common/modal/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import BootstrapModal from "react-bootstrap/Modal";
 
-const Modal = ({ onHide, children, title, fullscreen = false }) => {
+const Modal = ({
+  onHide,
+  children,
+  title,
+  fullscreen = false,
+  className = "",
+}) => {
   return (
-    <BootstrapModal show centered fullscreen={fullscreen} onHide={onHide}>
+    <BootstrapModal
+      show
+      centered
+      fullscreen={fullscreen}
+      onHide={onHide}
+      dialogClassName={className}
+    >
       <BootstrapModal.Header closeButton>
         <BootstrapModal.Title className="h5">{title}</BootstrapModal.Title>
       </BootstrapModal.Header>
