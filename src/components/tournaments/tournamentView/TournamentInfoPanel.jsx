6b9f2d486b52b35a801ae00f997241757e67a887
mostafab <mostafa0104@gmail.com>
2022-12-23 00:23:31 -0500

    Move modal to separate component

--- src/components/tournaments/tournamentView/TournamentInfoPanel.jsx
+++ src/components/tournaments/tournamentView/TournamentInfoPanel.jsx
@@ -6,7 +6,7 @@ import { labelWithValue } from "./utilities";
 
 const TournamentInfoPanel = ({ tournamentInfo }) => {
   return (
-    <Card title={tournamentInfo.name}>
+    <Card title={<h3>{tournamentInfo.name}</h3>}>
       {labelWithValue("Owner", tournamentInfo.directorId)}
       {labelWithValue("Date", tournamentInfo.tournamentDate)}
       {labelWithValue("Location", tournamentInfo.location)}
--- src/components/tournaments/tournamentView/TournamentPhasesPanel.jsx
+++ src/components/tournaments/tournamentView/TournamentPhasesPanel.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
-import { Row, Col } from "react-bootstrap";
 
-import Icon, { Warning } from "@components/common/icon";
+import Icon from "@components/common/icon";
 import Card from "@components/common/cards";
-import Modal from "@components/common/modal";
-import TeamsInPool from "@components/tournaments/tournamentView/pools/TeamsInPool";
+import AssignTeamPoolsModal from "@components/tournaments/tournamentView/pools/AssignTeamPoolsModal";
 
 const TournamentPhasesPanel = ({
   phases,
@@ -15,25 +11,6 @@ const TournamentPhasesPanel = ({
   teamsNotAssignedPools,
 }) => {
   const [showModal, setShowModal] = useState(false);
-  const renderPools = (phaseId) => {
-    const unassignedPool = { name: "No Assigned Pool", id: null };
-    const matching = pools.filter((p) => p.phaseId === phaseId);
-    return (
-      <Row>
-        <Col lg={3} md={6} sm={12} key="unassigned">
-          <TeamsInPool
-            pool={unassignedPool}
-            teams={teamsNotAssignedPools[phaseId] || []}
-          />
-        </Col>
-        {matching.map((p) => (
-          <Col lg={3} md={6} sm={12} key={p.id}>
-            <TeamsInPool pool={p} teams={poolTeams[p.id] || []} />
-          </Col>
-        ))}
-      </Row>
-    );
-  };
   if (!showModal) {
     return (
       <Card
@@ -41,7 +18,7 @@ const TournamentPhasesPanel = ({
         shadow
         title={
           <h5>
-            {"View/Assign Team Pools"}
+            View/Assign Team Pools
             {
               <Icon
                 className="float-end"
@@ -55,39 +32,13 @@ const TournamentPhasesPanel = ({
     );
   }
   return (
-    <Modal
-      className="TournamentPhasesModal"
+    <AssignTeamPoolsModal
+      phases={phases}
+      pools={pools}
+      poolTeams={poolTeams}
+      teamsNotAssignedPools={teamsNotAssignedPools}
       onHide={() => setShowModal(false)}
-      title="Team Pools"
-      size="xl"
-    >
-      <Card className="TournamentPhasesPanel" shadow={false} title={null}>
-        {phases.length > 0 && (
-          <Tabs defaultActiveKey={phases[0].id}>
-            {phases.map((p) => (
-              <Tab
-                key={p.id}
-                eventKey={p.id}
-                title={
-                  <>
-                    <span className="m-1">{p.name}</span>
-                    {teamsNotAssignedPools[p.id].length > 0 && (
-                      <Warning
-                        className="float-end"
-                        message="Some teams aren't assigned a pool."
-                      />
-                    )}
-                  </>
-                }
-              >
-                {renderPools(p.id)}
-              </Tab>
-            ))}
-            <Tab eventKey="new" key="new" title={<Icon name="Plus" />} />
-          </Tabs>
-        )}
-      </Card>
-    </Modal>
+    />
   );
 };
 
