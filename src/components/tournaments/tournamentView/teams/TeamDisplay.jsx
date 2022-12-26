b26855b1fdd5e3a1282f51c20da52165b4ae0f2d
mostafab <mostafa0104@gmail.com>
2022-12-26 12:43:08 -0500

    Add dropdown actions

--- src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
+++ src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 
 import Card from "@components/common/cards";
+import DropdownActions from "@components/common/DropdownActions";
 
 import TeamForm from "./TeamForm";
 import TeamMatches from "./TeamMatches";
@@ -10,6 +11,17 @@ const TeamDisplay = ({ team, matches, teams }) => (
   <Card>
     <Row>
       <Col lg={team.id ? 6 : 12} md={12} sm={12}>
+        {team.id && (
+          <DropdownActions
+            className="mb-3"
+            actions={[
+              {
+                label: <span className="text-danger">Delete Team</span>,
+                onClick: () => console.log(team),
+              },
+            ]}
+          />
+        )}
         <TeamForm team={team} />
       </Col>
       {team.id && (
