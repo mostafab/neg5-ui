883e6c357e4597771a480180a3a867aa1609c3df
mostafab <mostafa0104@gmail.com>
2022-12-26 21:12:42 -0500

    Formatting

--- src/components/tournaments/tournamentView/teams/NoMatches.jsx
+++ src/components/tournaments/tournamentView/teams/NoMatches.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Card from "@components/common/cards";
 
 const NoMatches = ({ team }) => (
-  <Card shadow={false} className="p-2">
-    <div className="d-flex justify-content-center">
-      {team.name} hasn't played in any matches yet. They'll show up here as
-      they're entered.
+  <Card shadow={false} className="p-3">
+    <div style={{ textAlign: "center" }}>
+      <p>
+        {team.name} hasn't played in any matches yet, but they'll show up here
+        as they're entered.
+      </p>
     </div>
   </Card>
 );
