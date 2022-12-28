7ba08f0d536aab7a489372064330340828968893
mostafab <mostafa0104@gmail.com>
2022-12-28 11:48:42 -0500

    collab actions

--- src/components/tournaments/tournamentView/collaborators/CollaboratorsList.jsx
+++ src/components/tournaments/tournamentView/collaborators/CollaboratorsList.jsx
@@ -3,6 +3,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import orderBy from "lodash/orderBy";
 
 import Pill from "@components/common/pill";
+import DropdownActions from "@components/common/DropdownActions";
 
 const CollaboratorsList = ({ collaborators, styles = {} }) => (
   <>
@@ -12,16 +13,29 @@ const CollaboratorsList = ({ collaborators, styles = {} }) => (
         collaborators,
         [(c) => (c.isAdmin ? 1 : 0), "userId"],
         ["desc", "asc"]
-      ).map((c) => (
-        <ListGroup.Item key={c.userId}>
-          {c.userId}
-          {c.isAdmin && (
-            <Pill className="float-end" type="success">
-              Admin
-            </Pill>
-          )}
-        </ListGroup.Item>
-      ))}
+      ).map((c) => {
+        const firstAction = {
+          label: c.isAdmin ? "Remove Admin Access" : "Grant Admin Access",
+        };
+        return (
+          <ListGroup.Item
+            key={c.userId}
+            className="d-flex justify-content-between"
+          >
+            <span>
+              {c.userId} {c.isAdmin && <Pill type="success">Admin</Pill>}
+            </span>
+            <DropdownActions
+              actions={[
+                firstAction,
+                {
+                  label: <span className="text-danger">Remove</span>,
+                },
+              ]}
+            />
+          </ListGroup.Item>
+        );
+      })}
     </ListGroup>
   </>
 );
