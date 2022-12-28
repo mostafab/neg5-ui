cafd3de41c501e27050c1f8c60c704084a8f2146
mostafab <mostafa0104@gmail.com>
2022-12-28 02:12:11 -0500

    Small refactoring

--- src/components/tournaments/tournamentView/collaborators/CollaboratorsModal.jsx
+++ src/components/tournaments/tournamentView/collaborators/CollaboratorsModal.jsx
@@ -8,7 +8,12 @@ import CollaboratorsList from "./CollaboratorsList";
 
 const CollaboratorsModal = ({ collaborators, onHide }) => (
   <Modal size="md" title="Update Collaborators" onHide={onHide}>
-    <UserSearch />
+    <UserSearch
+      placeholder="Add people"
+      filterFunction={(c) =>
+        !collaborators.find((collab) => collab.userId === c.id)
+      }
+    />
     <Info>
       All collaborators can add matches and players. Admin-level collaborators
       can edit and delete teams, players and matches they didn't create, as well
