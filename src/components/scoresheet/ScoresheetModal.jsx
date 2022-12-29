8dfdbf5e3e40c00d6cb539bcaaf1bc8017fcb539
mostafab <mostafa0104@gmail.com>
2022-12-28 23:59:12 -0500

    scoresheet state

--- src/components/scoresheet/ScoresheetModal.jsx
+++ src/components/scoresheet/ScoresheetModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 
 import Modal from "@components/common/modal";
@@ -6,21 +6,26 @@ import Card from "@components/common/cards";
 import ScoresheetStartForm from "./ScoresheetStartForm";
 
 const ScoresheetModal = ({ onHide, teams, rules, phases }) => {
+  const [scoresheetMetadata, setScoresheetMetadata] = useState(null);
   return (
     <Modal fullscreen title="Scoresheet" onHide={onHide}>
-      <Row>
-        <Col lg={4} md={3} />
-        <Col lg={4} md={6}>
-          <Card title="Enter a few fields to get started." className="mt-lg-5">
-            <ScoresheetStartForm
-              teams={teams}
-              phases={phases}
-              onSubmit={(values) => console.log(values)}
-            />
-          </Card>
-        </Col>
-        <Col lg={4} md={3} />
-      </Row>
+      {
+        !scoresheetMetadata && (
+          <Row>
+            <Col lg={4} md={3} />
+            <Col lg={4} md={6}>
+              <Card title="Fill out a few fields to get started." className="mt-lg-5 mt-md-5">
+                <ScoresheetStartForm
+                  teams={teams}
+                  phases={phases}
+                  onSubmit={(values) => setScoresheetMetadata(values)}
+                />
+              </Card>
+            </Col>
+            <Col lg={4} md={3} />
+          </Row>
+        )
+      }
     </Modal>
   );
 };
