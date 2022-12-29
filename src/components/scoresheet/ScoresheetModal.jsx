21a88b653da9e5a828a79878b9d3fffaa38818e8
mostafab <mostafa0104@gmail.com>
2022-12-29 00:12:16 -0500

    Title changes

--- src/components/scoresheet/ScoresheetModal.jsx
+++ src/components/scoresheet/ScoresheetModal.jsx
@@ -7,25 +7,37 @@ import ScoresheetStartForm from "./ScoresheetStartForm";
 
 const ScoresheetModal = ({ onHide, teams, rules, phases }) => {
   const [scoresheetMetadata, setScoresheetMetadata] = useState(null);
+
+  const getTitle = () => {
+    if (!scoresheetMetadata) {
+      return "Scoresheet";
+    }
+    const { team1Id, team2Id, round } = scoresheetMetadata;
+    const teamVsString = [team1Id, team2Id]
+      .map((teamId) => teams.find((t) => t.id === teamId).name)
+      .join(" vs ");
+    return `Round ${round}: ${teamVsString}`;
+  };
   return (
-    <Modal fullscreen title="Scoresheet" onHide={onHide}>
-      {
-        !scoresheetMetadata && (
-          <Row>
-            <Col lg={4} md={3} />
-            <Col lg={4} md={6}>
-              <Card title="Fill out a few fields to get started." className="mt-lg-5 mt-md-5">
-                <ScoresheetStartForm
-                  teams={teams}
-                  phases={phases}
-                  onSubmit={(values) => setScoresheetMetadata(values)}
-                />
-              </Card>
-            </Col>
-            <Col lg={4} md={3} />
-          </Row>
-        )
-      }
+    <Modal fullscreen title={getTitle()} onHide={onHide}>
+      {!scoresheetMetadata && (
+        <Row>
+          <Col lg={4} md={3} />
+          <Col lg={4} md={6}>
+            <Card
+              title="Fill out a few fields to get started."
+              className="mt-lg-5 mt-md-5"
+            >
+              <ScoresheetStartForm
+                teams={teams}
+                phases={phases}
+                onSubmit={(values) => setScoresheetMetadata(values)}
+              />
+            </Card>
+          </Col>
+          <Col lg={4} md={3} />
+        </Row>
+      )}
     </Modal>
   );
 };
