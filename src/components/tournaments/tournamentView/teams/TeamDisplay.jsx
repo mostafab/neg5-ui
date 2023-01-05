6029ada0d52fc0afb11046ce1d080ba8ac455070
mostafab <mostafa0104@gmail.com>
2023-01-05 11:11:45 -0500

    add oncancel

--- src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
+++ src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
@@ -24,6 +24,7 @@ const TeamDisplay = ({ team, matches, teams, onSubmitSuccess }) => {
                 className="mb-0"
                 message={`Are you sure you want to delete ${team.name}? This action is unrecoverable.`}
                 level="danger"
+                onCancel={() => setIsDeleting(false)}
               />
             </div>
           </Col>
