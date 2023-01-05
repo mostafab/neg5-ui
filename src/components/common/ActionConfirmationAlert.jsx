5a62e27f574fe6a75604488d5bcaa4217b581855
mostafab <mostafa0104@gmail.com>
2023-01-05 11:06:52 -0500

    Add common action confirmation alert

---
+++ src/components/common/ActionConfirmationAlert.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Alert from "@components/common/alerts";
+import Button from "@components/common/button";
+
+const ActionConfirmationAlert = ({
+  level,
+  onConfirm,
+  onCancel,
+  message,
+  className = "",
+  submitting = false,
+}) => (
+  <div>
+    <Alert level={level} className={className}>
+      <div className="d-lg-flex d-md-block justify-content-between">
+        <div className="pt-1 mb-2">{message}</div>
+        <div className="d-flex justify-content-end">
+          <Button
+            type="secondary"
+            className="me-3 shadow-sm"
+            onClick={onCancel}
+            submitting={submitting}
+          >
+            Cancel
+          </Button>
+          <Button
+            submitting={submitting}
+            type="primary shadow-sm"
+            onClick={onConfirm}
+          >
+            Confirm
+          </Button>
+        </div>
+      </div>
+    </Alert>
+  </div>
+);
+
+export default ActionConfirmationAlert;
--- src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
+++ src/components/tournaments/tournamentView/teams/TeamDisplay.jsx
@@ -1,54 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 
 import Card from "@components/common/cards";
 import DropdownActions from "@components/common/DropdownActions";
+import ActionConfirmationAlert from "@components/common/ActionConfirmationAlert";
 
 import TeamForm from "./TeamForm";
 import TeamMatches from "./TeamMatches";
 
 const TeamDisplay = ({ team, matches, teams, onSubmitSuccess }) => {
   const [readOnly, setReadOnly] = useState(true);
+  const [isDeleting, setIsDeleting] = useState(false);
+  useEffect(() => {
+    setIsDeleting(false);
+  }, [team.id]);
   return (
     <Card>
-      <Row>
-        <Col lg={team.id ? 6 : 12} md={12} sm={12}>
-          {team.id && readOnly && (
-            <DropdownActions
-              className="mb-3"
-              actions={[
-                {
-                  label: <span className="text-gray">Edit</span>,
-                  onClick: () => setReadOnly(false),
-                  separator: <hr className="mt-1 mb-1" />,
-                },
-                {
-                  label: <span className="text-danger">Delete Team</span>,
-                  onClick: () => console.log(team),
-                },
-              ]}
-            />
-          )}
-          <TeamForm
-            team={team}
-            readOnly={readOnly && team.id}
-            onCancel={team.id ? () => setReadOnly(true) : null}
-            onSubmitSuccess={
-              team.id
-                ? (result) => {
-                    setReadOnly(true);
-                    onSubmitSuccess(result);
-                  }
-                : null
-            }
-          />
-        </Col>
-        {team.id && (
-          <Col lg={6} md={12} sm={12}>
-            <TeamMatches team={team} matches={matches} teams={teams} />
+      {isDeleting && (
+        <Row>
+          <Col lg={12} md={12} sm={12}>
+            <div className="p-4">
+              <ActionConfirmationAlert
+                className="mb-0"
+                message={`Are you sure you want to delete ${team.name}? This action is unrecoverable.`}
+                level="danger"
+              />
+            </div>
           </Col>
-        )}
-      </Row>
+        </Row>
+      )}
+      {!isDeleting && (
+        <Row>
+          <Col lg={team.id ? 6 : 12} md={12} sm={12}>
+            {team.id && readOnly && !isDeleting && (
+              <DropdownActions
+                className="mb-3"
+                actions={[
+                  {
+                    label: <span className="text-gray">Edit</span>,
+                    onClick: () => setReadOnly(false),
+                    separator: <hr className="mt-1 mb-1" />,
+                  },
+                  {
+                    label: (
+                      <span className="text-danger">Delete {team.name}</span>
+                    ),
+                    onClick: () => setIsDeleting(true),
+                  },
+                ]}
+              />
+            )}
+            <TeamForm
+              team={team}
+              readOnly={readOnly && team.id}
+              onCancel={team.id ? () => setReadOnly(true) : null}
+              onSubmitSuccess={
+                team.id
+                  ? (result) => {
+                      setReadOnly(true);
+                      onSubmitSuccess(result);
+                    }
+                  : null
+              }
+            />
+          </Col>
+          {team.id && (
+            <Col lg={6} md={12} sm={12}>
+              <TeamMatches team={team} matches={matches} teams={teams} />
+            </Col>
+          )}
+        </Row>
+      )}
     </Card>
   );
 };
