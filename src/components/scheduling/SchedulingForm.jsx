1a93e17fa72db937d5cf6d39cfbb61aabdfb59a5
mostafab <mostafa0104@gmail.com>
2023-01-15 10:52:30 -0800

    Clear error on cancel

--- src/components/scheduling/SchedulingForm.jsx
+++ src/components/scheduling/SchedulingForm.jsx
@@ -157,7 +157,13 @@ const SchedulingForm = ({
       submitButtonText="Save"
       onSubmit={onSubmit}
       readOnly={readOnly}
-      onCancel={onCancel}
+      onCancel={() => {
+        setSubmitData({
+          error: null,
+          submitting: false,
+        });
+        onCancel();
+      }}
       submitting={submitData.submitting}
     >
       <ResetListener
