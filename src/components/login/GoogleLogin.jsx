687c59a8db50445f5f48719044363eeea9522f2b
mostafab <mostafa0104@gmail.com>
2023-01-20 19:25:36 -0500

    Google login loading icon

--- src/components/login/GoogleLogin.jsx
+++ src/components/login/GoogleLogin.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleLogin as ExtGoogleLogin } from "@react-oauth/google";
 
 import { attemptGoogleLogin } from "@api/login";
+import { setLoginCookie } from "@libs/cookies";
+
+import { Spinner } from "@components/common/icon";
 
 const GoogleLogin = ({ onLoginSuccess }) => {
+  const [loading, setLoading] = useState(false);
   const onSuccess = async (response) => {
+    setLoading(true);
     const token = (await attemptGoogleLogin(response)).token;
+    setLoginCookie(token);
+    setLoading(false);
+    onLoginSuccess();
   };
   return (
     <div>
-      <ExtGoogleLogin
-        text="Sign in with Googless"
-        theme="outline"
-        shape="rectangular"
-        onSuccess={onSuccess}
-        onError={() => {
-          console.log("Uh oh.");
-        }}
-      />
+      {loading && (
+        <div className="text-center">
+          <Spinner />
+        </div>
+      )}
+      {!loading && (
+        <ExtGoogleLogin
+          theme="outline"
+          shape="rectangular"
+          onSuccess={onSuccess}
+          onError={() => {
+            console.log("Uh oh.");
+          }}
+        />
+      )}
     </div>
   );
 };
